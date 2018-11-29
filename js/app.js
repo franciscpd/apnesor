997e4e2f74a1d41b9bc6d00673275b31a1b13330
Francisross Soares de Oliveira <franciscpd@gmail.com>
2018-11-29 19:54:14 -0200

    Animacoes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
+$(document).ready(function(){
+  fundoNavBar();
+});
+
 $(function() {
-  $(window).on("scroll", function() {
+  const fundoNavBar = function() {
     if ($(window).scrollTop() > $("#navbar").height()) {
       $("#navbar").addClass("navbar-bg");
     } else {
       $("#navbar").removeClass("navbar-bg");
     }
+  }
+
+  $(window).on("scroll", function() {
+    fundoNavBar();
   });
 });
 
