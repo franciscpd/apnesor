b14597c3125524172847950c0fbb761c8db9e598
Francisross Soares de Oliveira <franciscpd@gmail.com>
2018-11-29 23:16:48 -0200

    Formulário de contato

--- js/app.js
+++ js/app.js
@@ -14,6 +14,12 @@ const exibeBotao = function() {
   }
 }
 
+const limparForm = function() {
+  $("contact-form").each(function() {
+    this.reset();
+  });
+}
+
 $(document).ready(function(){
   fundoNavBar();
   exibeBotao();
@@ -23,6 +29,31 @@ $(function() {
   $(window).on("scroll", function() {
     fundoNavBar();
     exibeBotao();
+  });
+
+  $("#contact-form").submit(function(e) {
+    $("#contact-form-submit").prop('disabled', true);
+    e.preventDefault();
+
+    var email = {
+      nome: this.nome.value,
+      _replyto: this.email.value,
+      email: this.email.value,
+      telefone: this.telefone.value,
+      mensagem: this.mensagem.value,
+      _subject: 'Formulário de contato site'
+    }
+
+    $.ajax({
+      url: 'https://formspree.io/secretaria@apnesor.ong.br',
+      method: 'POST',
+      data: email,
+      dataType: 'json',
+      success: function() {
+        swal("Mensagem enviada =)", "Retornaremos o contato o mais breve possível", "success");
+        limparForm();
+      }
+    });
   });
 })
 
